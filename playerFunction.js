a35616a578bb4cb84ffd3a8a87d9326b577b7306
PhilippLottis <47495962+PhilippLottis@users.noreply.github.com>
2019-08-16 15:43:26 +0200

    Update playerFunction.js
    Dateien neu aufgeteilt

--- playerFunction.js
+++ playerFunction.js
@@ -36,25 +36,3 @@ function player(width, height, color, x, y, maxX, maxY, minX, minY) {
     }
   }
   
-  //Für Spieler 1
-function player1Movement() {
-  player1.speedX = 0;
-  player1.speedY = 0;
-  if (myGameArea.keys && myGameArea.keys[65]) {player1.speedX = -4;}
-  if (myGameArea.keys && myGameArea.keys[68]) {player1.speedX = 4;}
-  if (myGameArea.keys && myGameArea.keys[87] && player1.jump == true) {player1.speedY = -9;}
-  player1.newPos();
-  player1.update();
-}
-
-  //Für Spieler 2
-function player2Movement() {
-  player2.speedX = 0;
-  player2.speedY = 0;
-  if (myGameArea.keys && myGameArea.keys[37]) {player2.speedX = -4;}
-  if (myGameArea.keys && myGameArea.keys[39]) {player2.speedX = 4;}
-  if (myGameArea.keys && myGameArea.keys[38] && player2.jump == true) {player2.speedY = -9;}
-  player2.newPos();
-  player2.update();
-
-}
