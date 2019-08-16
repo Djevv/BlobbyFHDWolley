6e91288071e42610af4590f7398c72c39b6b63eb
PhilippLottis <47495962+PhilippLottis@users.noreply.github.com>
2019-08-16 15:43:29 +0200

    Create UpdateGameArea.js
    Dateien neu aufgeteilt

---
+++ UpdateGameArea.js
@@ -0,0 +1,71 @@
+function updateGameArea() {
+  myGameArea.clear();
+
+  //Für Netz + Boden
+  bodenp1.update();
+  bodenp2.update();
+  myNetz.update(); 
+
+  //player + ballMovement enthalten jeweils die Funktion .newPos() und .update()
+  //Für Spieler 1
+
+  player1.speedX = 0;
+  player1.speedY = 0;
+  if (myGameArea.keys && myGameArea.keys[65]) {player1.speedX = -4;}
+  if (myGameArea.keys && myGameArea.keys[68]) {player1.speedX = 4;}
+  if (myGameArea.keys && myGameArea.keys[87] && player1.jump == true) {player1.speedY = -9;}
+  player1.newPos();
+  player1.update();
+
+
+  //Für Spieler 2
+
+  player2.speedX = 0;
+  player2.speedY = 0;
+  if (myGameArea.keys && myGameArea.keys[37]) {player2.speedX = -4;}
+  if (myGameArea.keys && myGameArea.keys[39]) {player2.speedX = 4;}
+  if (myGameArea.keys && myGameArea.keys[38] && player2.jump == true) {player2.speedY = -9;}
+  player2.newPos();
+  player2.update();
+
+
+  //Für Ball1
+  ball1.speedY = 0;
+  if (ball1.collisionp1(player1)) {
+  ball1.gravitySpeed = (5) * (((ball1.y + (ball1.height / 2)) - (player1.y + (player1.height / 2))) / 25);
+  ball1.speedX = (((ball1.x + (ball1.width / 2)) - (player1.x + (player1.width / 2))) / 25) * 5
+  ball1.speedY +=  -10;
+  };
+  if (ball1.collisionp1(player2)) {
+  ball1.gravitySpeed = (5) * (((ball1.y + (ball1.height / 2)) - (player2.y + (player2.height / 2))) / 25);
+  ball1.speedX = (((ball1.x + (ball1.width / 2)) - (player2.x + (player2.width / 2))) / 25) * 5
+  ball1.speedY +=  -10;
+  };
+  if (ball1.collisionp1(myNetz)) {
+    if ((ball1.y + (ball1.height / 2)) <= myNetz.y){
+      ball1.gravitySpeed += (-1);
+    }
+  //ball1.gravitySpeed = (-5) + ((Math.abs(((ball1.x + (ball1.width / 2)) - (myNetz.x + (myNetz.width / 2))) / 25)) * (2));
+    else{
+      ball1.speedX = (((ball1.x + (ball1.width / 2)) - (myNetz.x + (myNetz.width / 2))) / 25) * 5
+    }
+
+  //ball1.speedY +=  -10;
+  };
+
+  ball1.newPos();
+  ball1.update();
+
+  
+  if(bodenp1.collisionBall()){ 
+
+  };
+
+  if(bodenp2.collisionBall()){
+
+  };
+  
+
+  document.getElementById("X").innerHTML = player1.x;
+  document.getElementById("Y").innerHTML = player1.y;
+}
